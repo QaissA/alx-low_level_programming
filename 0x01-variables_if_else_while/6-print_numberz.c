172262a32520cc4ce42b780d812feb1211e3d0b3
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-07-14 15:12:32 -0700

    adding the 6th c program

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+"include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int void(main)
+{
+int number = 0;
+while (number <= 9)
+{
+putchar(number + '0');
+number++;
+}
+putchar('\n');
+return (0);
+}
