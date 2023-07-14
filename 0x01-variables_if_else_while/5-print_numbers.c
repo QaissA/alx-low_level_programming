f03c7e3afa7cd5a50f62d2e71a8e9ad527ffc881
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-07-14 15:00:33 -0700

    adding the fifth script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar('0'+ digit);
+digit++;
+}
+puthcar('\n');
+return (0);
+}
