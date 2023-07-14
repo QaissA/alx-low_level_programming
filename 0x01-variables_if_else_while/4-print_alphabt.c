098683f3e731c149e97a19b1f8662e07c9383e4b
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-07-14 14:49:44 -0700

    up c 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+char letter='a';
+while (letter <= 'z')
+{
+if (letter !='q' && letter !='e')
+{
+putchar(letter);
+}
+letter++;
+}
+putchar("\n");
+return (0);
+}
