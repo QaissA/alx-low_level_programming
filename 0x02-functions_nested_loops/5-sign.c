2788e1415b6fe82a39e74769794aa3352c1fd007
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-07-18 14:22:04 -0700

    3rd challenge: 6th script

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf('+');
+_putchar('+');
 return (1);
 } else if (n == 0)
 {
-printf('0');
+_putchar(48);
 return (0);
 } else
 {
-printf('-');
+_putchar('-');
 return (-1);
 }
 }
