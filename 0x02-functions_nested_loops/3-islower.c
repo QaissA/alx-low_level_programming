e7a347c19f07fa621bca319b424766c66dd1d8b9
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-07-18 13:51:09 -0700

    3rd challenge : 4th program

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower: return 1 if c is lower, 0 if c is uppercase
+ * c : the character to check
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c<='z')
+{
+return (1);
+}
+else
+{
+return (1);
+}
+}
