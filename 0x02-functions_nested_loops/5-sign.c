7a7998ac3178880044e80310497e9b0fc9162a6a
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-07-18 14:27:43 -0700

    3rd challenge: 6th script

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- * @n: the number to be checker
- * Return : 1 and + if number greater then zero, 
- * zero if number equal zero 
- * or -1 and - if number lower then zero
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -13,11 +12,12 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-} else if (n == 0)
+} 
+else if (n == 0)
 {
 _putchar(48);
 return (0);
-} 
+}
 else
 {
 _putchar('-');
