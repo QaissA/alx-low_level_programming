b61d5e1a55b55e333c8005b1a51b3dc1fdec97b8
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-08-24 16:47:24 -0700

    challenge 100-

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_before_main - function to print before main starts
+ * Return: no return value
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
