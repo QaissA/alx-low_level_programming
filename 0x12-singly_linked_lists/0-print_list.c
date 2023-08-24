b6d2160e7b02b84fce0dbfeb7c485bc8ff72318f
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-08-24 16:21:50 -0700

    challenge for data structure : linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - funtion that will print all elements of list given as parameter.
+ * @h : is header to the linked list
+ * Return : return the number of nodes in the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
