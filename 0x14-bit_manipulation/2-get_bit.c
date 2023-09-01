4ed014bf1ae2ce9b0e578c59b4c51561d381c2e4
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-09-01 12:17:18 -0700

    3rd challenge 2-

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* get_bit - returns the value of a bit at a given index.
+* @n: is anumber
+* @index: is an index
+* Return: the value of the bit at index
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+
+
+
+
+return ((n >> index) & 1);
+
+}
