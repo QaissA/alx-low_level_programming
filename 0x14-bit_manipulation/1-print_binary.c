3ba3279b2322d729dccc1b3cd3edc1c753ca5afc
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-09-01 12:15:37 -0700

    second challange 1-

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number.
+* @n: is a number
+*/
+
+void print_binary(unsigned long int n)
+{
+int f = 0;
+int i;
+int x;
+
+if (n == 0)
+_putchar('0');
+
+for (i = 63; i >= 0; i--)
+{
+x = (n >> i);
+if (x != 0)
+f = 1;
+if (f != 0)
+{
+if (x & 1)
+_putchar('1');
+else
+_putchar('0');
+}
+}
+}
