176d414712b8a97fe1d050483e2ec8e3aac9099b
QAISS Abdelhamid <qaissabdelhamid@gmail.com>
2023-09-04 17:00:33 -0700

    4th challenge 3-

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+* f_close - close the files.
+* @x: the value of the file descriptor.
+* @i: the value of the file descriptor.
+*/
+
+void f_close(int x, int i)
+{
+if (close(x) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", x);
+exit(100);
+}
+
+if (close(i) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+exit(100);
+}
+
+}
+
+/**
+* main - copies the content of a file to another file
+* @argv: number of argument.
+* @argc: the argument (files);
+* Return: 1
+*/
+
+int main(int argc, char *argv[])
+{
+int x, i;
+ssize_t v = 1024, n;
+char c[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97); }
+
+x = open(argv[1], O_RDONLY);
+if (x == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98); }
+i = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (i == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99); }
+while (v == 1024)
+{
+v = read(x, c, 1024);
+if (v == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98); }
+
+n = write(i, c, v);
+if (n == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99); }
+}
+
+f_close(x, i);
+return (0);
+}
